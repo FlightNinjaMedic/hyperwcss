0b4a897ceb5fedd967780bb7702136a879f0c08a
Hector Lozano <hlozano2@cnm.edu>
2016-10-05 15:26:02 -0600

    Added index and css files

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>The Vitamin Store</title>
+		<link rel="styleshee" type="text/css" href="css/special.css"/>
+	</head>
+	<body>
+		<table summary="Home Page">
+			<tr>
+				<td width="20%"><a href="index.html">Home</a> </td>
+				<td width="20%"><a href="buy.html">Buy Now</a> </td>
+				<td width="20%"><a href="products.html">Products and Services</a> </td>
+				<td width="20%"><a href="yourhealth.html"></a> </td>
+				<td width="20%"><a href="about.html">About Us</a> </td>
+			</tr>
+			<tr>
+				<td colspan="3">
+					<h1>Welcome to the Vitamin Store: Protecting Your Health Naturally!</h1>
+					<p>Founded April 30, 2016 The Vitamin Store is veteran owned and operated.
+						We offer the best herbal medication that money can buy</p>
+					<p>Many Companies that talk about Customer Service fail to deliver.
+						We've Served Our Country, Now We Are Redy To Serve You!</p>
+				</td>
+			</tr>
+			<tr>
+				<td colspan="3">The Vitamin Store &copy; 2016 NinjaMedic LLC</td>
+				<td><a href="contact.html">Contact Us</a> </td>
+				<td><a href="sitemap.html">Site Map</a> </td>
+			</tr>
+		</table>
+	</body>
+</html>
